b398b387a9e9a66dc8759127fa3d8a0232b2353a
Carlos Ortega Morales <caomboy19@gmail.com>
2017-09-14 16:11:41 -0500

    Add project files.

---
+++ LCD/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LCD.Models;
+
+namespace LCD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Transform(int? number)
+        {
+            if (number == null)
+            {
+                return View();
+            }
+
+            var pantalla = new Display();
+            pantalla.number = number.ToString();
+            ViewData["Number"] = pantalla.GetLCDrepresentation();
+
+            return View();
+        }
+    }
+}
---
+++ LCD/Models/Display.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LCD.Models
+{
+    public class Display
+    {
+        public string number { get; set; }
+        public string[,] charNumbers = new String[3, 10] {
+            { "._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._." },
+            { "|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|" },
+            { "|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|" }
+        };
+
+        public string GetLCDrepresentation()
+        {
+            int numberLenght = number.Length;
+            string line = "";
+            for (int i = 0; i < 3; i++)
+            {
+                line += getStructureLine(i, numberLenght) + Environment.NewLine;
+            }
+            return line;
+        }
+
+        private string getStructureLine(int lineNumber, int numberLenght)
+        {
+            int digit = 0;
+            string line = "";
+            for (int o = 0; o < numberLenght; o++)
+            {
+                digit = Int32.Parse(number[o].ToString());
+                line += charNumbers[lineNumber, digit];
+                line += "   ";
+            }
+            return line;
+        }
+    }
+}
